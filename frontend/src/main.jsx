ad86780ac4b97ae3fb6065d82da841fc34a0f4da
Mohamed Abdulwahab <mabdulwahab.dev@gmail.com>
2023-07-22 19:58:37 -0700

    added tailwind styling

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import '../src/index.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import '../firebase-config.js';
 import { Provider } from 'react-redux';
 import store from './store/store.js';
