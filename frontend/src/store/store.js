46d8020e9838ebcf46a77c08e99ab421273dc311
Mohamed Abdulwahab <mabdulwahab.dev@gmail.com>
2023-07-25 19:53:52 -0700

    added timer slice reducer

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { apiSlice } from './apiSlices/apiSlice';
 import authSliceReducer from './slices/authSlice';
+import timerSliceReducer from './slices/timerSlice';
 
 const store = configureStore({
   reducer: {
     [apiSlice.reducerPath]: apiSlice.reducer,
     auth: authSliceReducer,
+    timer: timerSliceReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(apiSlice.middleware),
