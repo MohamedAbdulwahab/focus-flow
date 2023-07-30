af01f84c39e33003b23dc266ad41d3585adb806e
Mohamed Abdulwahab <mabdulwahab.dev@gmail.com>
2023-07-30 00:50:34 -0700

    added routes to update display name and update email address

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,12 +3,15 @@ import {
   registerUser,
   getUserInfo,
   updateDisplayName,
+  updateEmail,
 } from '../controllers/userController.js';
+import { ensureAuth } from '../middleware/ensureAuth.js';
 
 const router = express.Router();
 
 router.route('/').post(registerUser);
-router.route('/:id').put(updateDisplayName);
 router.route('/login').post(getUserInfo);
+router.route('/:id').put(updateDisplayName);
+router.route('/:id/email').put(updateEmail);
 
 export default router;
