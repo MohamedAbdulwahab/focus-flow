dcedac0a864fedc17e96ecc47d817b740ba4f654
Mohamed Abdulwahab <mabdulwahab.dev@gmail.com>
2023-07-31 17:04:17 -0700

    enhanced the logic to check tokens from logged in users

--- backend/middleware/ensureAuth.js
+++ backend/middleware/ensureAuth.js
@@ -1,7 +1,17 @@
 import { admin } from '../config/firebase-config.js';
 
 const ensureAuth = async (req, res, next) => {
-  const token = req.headers.token.split(' ')[1];
+  // variable to store the token value.
+  let token;
+
+  // check if a token is provided.
+  if (!req.headers.token) {
+    // no token has been provided: return an error message.
+    return res.status(404).json({ message: 'Error: invalid token' });
+  } else {
+    // token has been provided: store it in the token variable.
+    token = req.headers.token.split(' ')[1];
+  }
 
   try {
     const decodeValue = await admin.auth().verifyIdToken(token);
