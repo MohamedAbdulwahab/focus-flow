6e3e58c90521f0b5d4d0735fee3d2b45350b7019
Mohamed Abdulwahab <mabdulwahab.dev@gmail.com>
2023-08-02 11:04:50 -0700

    deleted unnecessary code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,6 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import ProfilePage from './pages/ProfilePage';
 import PrivateRoute from './components/PrivateRoute';
-// import AdminRoute from './components/AdminRoute';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
